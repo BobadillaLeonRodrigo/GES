0a0ca38abd61be3a475285e245ec19bd46127923
BobadillaLeonRodrigo <al222110815@gmail.com>
2023-04-02 16:07:44 -0600

    Cambio de Estilos

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,41 +1,75 @@
 @include('layout.scripts')
 @include('layout.header')
-<section class="bg-success">
-    <div class="container-fluid p-4">
-        <div class="row justify-content-center align-items-center">
-            <div class="col-xl-5 col-md-6 col-lg-3">
-                <div class="card shadow-2-strong border-dark border-3 border rounded-5">
-                    <!-- Parte del login card-body -->
-                    <div class="card-body p-1 text-center bg-white rounded-5">
-                        <h3 class="mb-5">Inicio de Sesion </h3>
-                            <form action="{{route('validar')}}" method="POST">
-                                {{csrf_field()}}
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="typeEmailX-2">Correo Electronico</label>
-                                            <input  type="email" name="email" id="email"
-                                                class="text-white form-control form-control-lg border-dark bg-success border-3 rounded-4"/>
-                                        @if($errors ->first('email'))
-                                        <p class='text-danger'>{{$errors->first('email')}} </p>
-                                        @endif
-                                    </div>
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="typePasswordX-2">Contraseña</label>
-                                                <input type="password" name="passw" id="passw"
-                                                    class="text-white form-control form-control-lg border-dark bg-success border-3 rounded-4"/>
-                                            @if($errors ->first('passw'))
-                                            <p class='text-danger'>{{$errors->first('passw')}} </p>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+            <title>Inicio de Sesión</title>
+                <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+                    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+<body class="bg-success">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-6">
+                            <div class="card shadow-lg border-dark border border-3 rounded-lg mt-5 rounded-3">
+                                <div class="card-header">
+                                    <h3 class="text-center font-weight-light my-4">Inicio de Sesión</h3>
+                                </div>
+                                    <div class="card-body">
+                                        <form action="{{route('validar')}}" method="POST">
+                                            {{csrf_field()}}
+                                        <div class="form-floating mb-3">
+                                            <input
+                                                class="form-control"
+                                                type="email"
+                                                name="email"
+                                                id="email"
+                                                placeholder="name@example.com"
+                                            />
+                                            @if($errors ->first('email'))
+                                                <p class='text-danger'>{{$errors->first('email')}} </p>
                                             @endif
+                                            <label for="inputEmail">Correo Electronico</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input
+                                                class="form-control"
+                                                name="passw"
+                                                id="passw"
+                                                type="password"
+                                                placeholder="Password"
+                                            />
+                                            @if($errors ->first('passw'))
+                                                <p class='text-danger'>{{$errors->first('passw')}} </p>
+                                            @endif
+                                            <label for="inputPassword">Contraseña</label>
                                         </div>
                                             @if(Session::has('mensaje'))
                                                 <div class="alert alert-success">{{Session::get('mensaje')}}</div>
                                             @endif
-                                                    <button class="btn btn-success border-dark border-3 btn-lg btn-block rounded-3"
-                                                        type="submit">Ingresar</button>
-                            </form>
+                                            <button class="btn btn-success text-center border border-dark" type="submit"><em>Ingresar</em></button>
+                                    </div>
+                                        </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
         </div>
+            </main>
     </div>
-</section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
+</body>
+</html>
 @include('layout.footer')
--- resources/views/login/registro_u.blade.php
+++ resources/views/login/registro_u.blade.php
@@ -1,47 +1,118 @@
 @include('layout.scripts')
 @include('layout.header')
-<section class="bg-success p-4">
-<div class="container-fluid col-md-5 bg-white border-dark border-4 border rounded-5">
-<h1 class="text-center mb-3"><em>Registro</em></h1>
-    <form class="row" method="POST" action="{{route('registro_usuarios')}}">
-        {{csrf_field()}}
-        <div class="col-md-8 offset-md-2">
-            <label for="Nombre" class="form-label">Nombre de Usuario</label>
-            <input type="name" class="form-control rounded-5 border-dark text-white bg-success" placeholder="Ingrese Su Nombre" id="nombre_u" name="nombre_u">
-            @if($errors ->first('nombre_u'))
-                <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
-            @endif
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Registro de Usuarios</title>
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-success">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="card shadow-lg border-4 border-dark rounded-3 rounded-lg mt-3">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Crea un Cuenta</h3></div>
+                                    <div class="card-body">
+                                        <form class="row" method="POST" action="{{route('registro_usuarios')}}">
+                                        {{csrf_field()}}
+                                            <div class="row mb-4">
+                                                <div class="col-md-4">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input
+                                                            class="form-control"
+                                                            id="nombre_u"
+                                                            name="nombre_u"
+                                                            type="name"
+                                                            placeholder="Enter your first name"
+                                                        />
+                                                        @if($errors ->first('nombre_u'))
+                                                            <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
+                                                        @endif
+                                                        <label for="inputFirstName">Nombre</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-floating">
+                                                        <input
+                                                            class="form-control"
+                                                            id="app_u"
+                                                            name="app_u"
+                                                            type="name"
+                                                            placeholder="Enter your last name"
+                                                        />
+                                                        @if($errors ->first('nombre_u'))
+                                                            <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
+                                                        @endif
+                                                        <label for="inputLastName">Apellido Paterno</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-floating">
+                                                        <input
+                                                            class="form-control"
+                                                            id="apm_u"
+                                                            name="apm_u"
+                                                            type="name"
+                                                            placeholder="Enter your last name"
+                                                        />
+                                                        @if($errors ->first('nombre_u'))
+                                                            <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
+                                                        @endif
+                                                        <label for="inputLastName">Apellido Materno</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input
+                                                    class="form-control"
+                                                    id="email"
+                                                    name="email"
+                                                    type="email"
+                                                    placeholder="name@example.com"
+                                                />
+                                                @if($errors ->first('email'))
+                                                    <p class='text-danger'>{{$errors->first('email')}} </p>
+                                                @endif
+                                                <label for="inputEmail">Correo Electronico</label>
+                                            </div>
+                                            <div class="row mb-3">
+                                                    <div class="form-floating mb-3">
+                                                        <input
+                                                            class="form-control"
+                                                            id="passw"
+                                                            name="passw"
+                                                            type="password"
+                                                            placeholder="Create a password"
+                                                        />
+                                                        @if($errors ->first('nombre_u'))
+                                                            <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
+                                                        @endif
+                                                        <label for="inputPassword">Contraseña</label>
+                                                    </div>
+                                            </div>
+                                            <div class="mt-4 mb-0">
+                                                <div class="d-grid"><button class="btn btn-success btn-block border border-dark" type="submit"><em>Crear Cuenta</em></button></div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
         </div>
-        <div class="col-md-8 offset-md-2">
-            <label for="Nombre" class="form-label">Apellido Paterno del Usuario</label>
-            <input type="name" class="form-control rounded-5 border-dark text-white bg-success" placeholder="Ingrese Su Nombre" id="app_u" name="app_u">
-            @if($errors ->first('nombre_u'))
-                <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
-            @endif
-        </div>
-        <div class="col-md-8 offset-md-2" >
-            <label for="Nombre" class="form-label">Apellido Materno del Usuario</label>
-            <input type="name" class="form-control rounded-5 border-dark text-white bg-success" placeholder="Ingrese Su Nombre" id="apm_u" name="apm_u">
-            @if($errors ->first('nombre_u'))
-                <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
-            @endif
-        </div>
-        <div class="col-md-8 offset-md-2" >
-            <label for="Email" class="form-label">Correo Electronico</label>
-            <input type="text" class="form-control rounded-5 border-dark text-white bg-success" placeholder="Ingrese Correo Electronico" id="email" name="email">
-            @if($errors ->first('email'))
-                <p class='text-danger'>{{$errors->first('email')}} </p>
-            @endif
-        </div>
-        <div class="col-md-8 offset-md-2">
-            <label for="password" class="form-label">Contraseña</label>
-            <input type="password" class="form-control rounded-5 border-dark text-white bg-success" id="passw" placeholder="Contraseña Max. 8 Digitos" name="passw">
-            @if($errors ->first('nombre_u'))
-                <p class='text-danger'>{{$errors->first('nombre_u')}} </p>
-            @endif
-        </div>
-            <button class="btn btn-dark col-md-6 my-3 offset-md-3 rounded-5 border-dark text-white bg-success" type="submit">Registrarse</button>
-    </form>
-</div>
-</section>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
+    </body>
+</html>
 @include('layout.footer')
